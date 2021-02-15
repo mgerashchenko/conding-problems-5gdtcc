a50f259b87234f456acd28605cc32795d734e8f3
mgerashchenko <mgerashchenko@gmail.com>
2021-02-15 18:03:42 -0500

    Functiononal Programming Promise

---
+++ FFF Functional Programming/Promisies.ts
@@ -0,0 +1,25 @@
+console.clear();
+
+// Creates new Promise
+const fetch = (val) => {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res(val);
+        }, val*1000);
+    })
+}
+
+fetch(2).then( (val) => {
+    console.log('res', val);
+})
+
+// Make requests in paralle;
+Promise.all([
+    fetch(1),
+    fetch(2),
+    fetch(3),
+]).then( respones => {
+    respones.forEach((val)=>{
+        console.log(val);
+    })
+});
