57512ed2fbba2f0779e5852e92262ab093766515
mgerashchenko <mgerashchenko@gmail.com>
2021-02-27 22:33:21 -0500

    1 file modified

--- Competetive Programming/DP-2-Palindromic Substrings.ts
+++ Competetive Programming/DP-2-Palindromic Substrings.ts
@@ -1,2 +1,4 @@
 // 647. Palindromic Substrings
 // https://leetcode.com/problems/palindromic-substrings/
+
+//https://www.geeksforgeeks.org/overlapping-subproblems-property-in-dynamic-programming-dp-1/
