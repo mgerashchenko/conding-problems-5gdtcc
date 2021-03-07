326b542a1c4ae80d037ed183153f5f8f14432d40
mgerashchenko <mgerashchenko@gmail.com>
2021-03-06 22:21:49 -0500

    Maximum Level Sum of a Binary Tree

--- Competetive Programming/DFS-1-MaximumLevelSumOfBinaryTree.ts
+++ Competetive Programming/DFS-1-MaximumLevelSumOfBinaryTree.ts
@@ -1,2 +1,34 @@
 // https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
 // 1161. Maximum Level Sum of a Binary Tree
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxLevelSum = function(root) {
+  if (root == null) return 0;
+
+  let level = 0;
+  let minLevel = Infinity;
+  let maxSum = -Infinity;
+  let queue = [root];
+
+  while (queue.length) {
+    level++;
+    let sum = queue.reduce((prev, cur) => prev + cur.val, 0);
+    if (sum > maxSum) {
+      minLevel = level;
+      maxSum = sum;
+    }
+
+    let newQueue = [];
+    while (queue.length) {
+      let node = queue.shift();
+      if (node.left) newQueue.push(node.left);
+      if (node.right) newQueue.push(node.right);
+    }
+    queue = [...newQueue];
+  }
+
+  return minLevel;
+};
