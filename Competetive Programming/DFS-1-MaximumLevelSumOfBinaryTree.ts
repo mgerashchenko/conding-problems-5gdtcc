5f119c7662d5836440314725d67971d4c23e93ce
mgerashchenko <mgerashchenko@gmail.com>
2021-03-06 23:53:35 -0500

    DFS 1

--- Competetive Programming/DFS-1-MaximumLevelSumOfBinaryTree.ts
+++ Competetive Programming/DFS-1-MaximumLevelSumOfBinaryTree.ts
@@ -9,25 +9,25 @@ var maxLevelSum = function(root) {
   if (root == null) return 0;
 
   let level = 0;
-  let minLevel = Infinity;
+  let minLevel = 0;
   let maxSum = -Infinity;
   let queue = [root];
 
   while (queue.length) {
+    let length = queue.length;
     level++;
-    let sum = queue.reduce((prev, cur) => prev + cur.val, 0);
+    let sum = 0;
+
+    for (let i = 0; i < length; i++) {
+      let node = queue.shift();
+      sum += node.val;
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
     if (sum > maxSum) {
       minLevel = level;
       maxSum = sum;
     }
-
-    let newQueue = [];
-    while (queue.length) {
-      let node = queue.shift();
-      if (node.left) newQueue.push(node.left);
-      if (node.right) newQueue.push(node.right);
-    }
-    queue = [...newQueue];
   }
 
   return minLevel;
