9f191ba1749a56b13d3027b82d994f108e402e84
mgerashchenko <mgerashchenko@gmail.com>
2021-03-09 00:05:24 -0500

    1 file modified

--- Competetive Programming/DFS-1-MaximumLevelSumOfBinaryTree.ts
+++ Competetive Programming/DFS-1-MaximumLevelSumOfBinaryTree.ts
@@ -63,20 +63,20 @@ var maxLevelSum = function(root) {
     //return Object.keys(levels).reduce( (a,b) => levels[a] > levels[b] ? a : b)[0];
 
     var maxLevelSum = function(root) {
-      const levels = [-Infinity];
-      (function DFS(node, level) {
-        if (!node) return;
+    const levels = [];
+    (function DFS(node, level){
+        if(!node) return;
+        if(levels.length === level) levels.push(0);
+        
+        const {left, right, val} = node;
+        levels[level] += val;
+        
+        level++;
+        DFS(left, level);
+        DFS(right, level);
+    })(root, 0)
 
-        levels.length === level && levels.push(0);
-
-        levels[level] += node.val;
-
-        DFS(node.left, level + 1);
-        DFS(node.right, level + 1);
-      })(root, 1);
-
-      const max = Math.max(...levels);
-      return levels.indexOf(max);
-    };
+    let max = Math.max(...levels);
+    return levels.indexOf(max)+1;
   };
 };
