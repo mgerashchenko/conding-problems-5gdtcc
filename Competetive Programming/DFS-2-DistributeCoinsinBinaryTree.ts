9ef90f351edfdfb85e7b8f7eef443a93c3f45445
mgerashchenko <mgerashchenko@gmail.com>
2021-03-14 17:53:20 -0400

    DFS-2

--- Competetive Programming/DFS-2-DistributeCoinsinBinaryTree.ts
+++ Competetive Programming/DFS-2-DistributeCoinsinBinaryTree.ts
@@ -2,3 +2,29 @@
 
 // 979. Distribute Coins in Binary tree
 // https://leetcode.com/problems/distribute-coins-in-binary-tree/
+
+// Screen shot to understand the Pattern https://youtu.be/ZhGtKbdrAhI?t=161
+
+// First of it is DFS
+// Need to understand the type preOrder, inOrder, postOrder
+// It is postOrder because we need to calculte after the recursion(DFS call)
+// Here are 3 parts of a calculation.
+// The first problem is to count the value of the leaf node.val - 1
+// Then need to count the children values, Math.abs(left) + Math.abs(right)
+// That is a small graph, and it is a subproblem itself, so we need to figure out what to return to the top form.
+// Last but not least, subproblem is return left + right + node.val - 1.
+// If it is 0 it means that a small subproblem is balanced.
+
+var distributeCoins = function(root) {
+  let count = 0;
+
+  (function DFS(node) {
+    if (!node) return 0;
+    const left = DFS(node.left);
+    const right = DFS(node.right);
+    count += Math.abs(left) + Math.abs(right);
+    return left + right + node.val - 1;
+  })(root);
+
+  return count;
+};
--- Competetive Programming/DFS-2-MaximumDifferenceBetweenNodeAndAncestor.ts
+++ Competetive Programming/DFS-2-MaximumDifferenceBetweenNodeAndAncestor.ts
@@ -1,3 +1,2 @@
 // https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/
 // 1026. Maximum Difference Between Node and Ancestor
-
