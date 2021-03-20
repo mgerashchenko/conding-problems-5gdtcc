39e2f963ba1201b9c62baf7ac4351149cc840bcf
mgerashchenko <mgerashchenko@gmail.com>
2021-03-20 13:11:43 -0400

    2P-1

---
+++ Competetive Programming/2P-1-PartitionLabels.ts
@@ -0,0 +1,2 @@
+// https://leetcode.com/problems/partition-labels/
+// Partition Labels
--- Competetive Programming/DFS-2-PopulatingNextRightPointersInEachNode.ts
+++ Competetive Programming/DFS-2-PopulatingNextRightPointersInEachNode.ts
@@ -1,7 +1,6 @@
 // https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
 // 116. Populating Next Right Pointers in Each Node
 
-
 /**
  * // Definition for a Node.
  * function Node(val, left, right, next) {
@@ -17,29 +16,28 @@
  * @return {Node}
  */
 var connect = function(root) {
-    if(!root) return null;
-    const queue = [root];
+  if (!root) return null;
+  const queue = [root];
 
-    //BFS
-    while(queue.length){
-        let prev = null;
-        let curLength = queue.length;
-        for(let i=0;i<curLength;i++){
-            let node = queue.shift();
-            node.next = null;
-            if(prev) {
-                 prev.next = node;
-            }
-            prev = node;
-            let {left, right} = node;
-            
-            left && queue.push(left);
-            right && queue.push(right);
-        }
+  //BFS
+  while (queue.length) {
+    let prev = null;
+    let curLength = queue.length;
+    for (let i = 0; i < curLength; i++) {
+      let node = queue.shift();
+      node.next = null;
+      if (prev) {
+        prev.next = node;
+      }
+      prev = node;
+      let { left, right } = node;
+
+      left && queue.push(left);
+      right && queue.push(right);
     }
+  }
 
-    return root;
+  return root;
 };
-
 
 //DFS
