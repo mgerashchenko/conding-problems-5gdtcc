8512d52a240376ac9f37d654bfaaf4bfe3887529
mgerashchenko <mgerashchenko@gmail.com>
2021-03-22 23:27:51 -0400

    up

--- Competetive Programming/2P-1-PartitionLabels.ts
+++ Competetive Programming/2P-1-PartitionLabels.ts
@@ -6,32 +6,27 @@
  * @return {number[]}
  */
 var partitionLabels = function(S) {
-    const partions = [];
-    const maxIndexs = {};
+    const res = [];
+    const map = new Map();
     
     // Map max indexs
     for(let i=0;i<S.length;i++) {
-        maxIndexs[S[i]] = i;
+        map.set(S[i], i);
     }
-    
-    let maxIndex = maxIndexs[S[0]];
-    let lastIndex = 0;
+
+    let max = 0;
+    let last = 0;
     for(let i=0;i<S.length;i++) {
-        let char = S[i];
-        if(i===maxIndex) {
-            // add 1 to index to get length
-            partions.push(i - lastIndex + 1);
-            // count from 0 index
-            lastIndex = i + 1;
-            maxIndex = maxIndexs[S[i+1]];
-            continue;
-        }
-
-        if(maxIndexs[char] > maxIndex) {
-            maxIndex = maxIndexs[char];
-        }
+        let cur = S[i];        
+        max = Math.max(max, map.get(cur));
+        if(i<max) continue;
+        
+        // add 1 to index to get length
+        res.push(i - last + 1);
+        // count from 0 index
+        last = i + 1;
     }
 
-    return partions;
+    return res;
 };
 
