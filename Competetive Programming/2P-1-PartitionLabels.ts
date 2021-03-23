2b292e4c225770a44dcbc31a6dc375ca9480658f
mgerashchenko <mgerashchenko@gmail.com>
2021-03-22 21:49:06 -0400

    2P-1

--- Competetive Programming/2P-1-PartitionLabels.ts
+++ Competetive Programming/2P-1-PartitionLabels.ts
@@ -1,2 +1,31 @@
 // https://leetcode.com/problems/partition-labels/
 // Partition Labels
+
+/**
+ * @param {string} S
+ * @return {number[]}
+ */
+var partitionLabels = function(S) {
+  const partions = [];
+
+  const { length } = S;
+  let max = 0;
+  let prevMaxIndex = 0;
+
+  for (let i = 0; i < length; i++) {
+    let cur = S[i];
+
+    for (let j = 0; j < length; j++) {
+      if (S[j] === cur && j > max) {
+        max = j;
+      }
+    }
+
+    if (i == max) {
+      let prev = partions.reduce((prev, cur) => prev + cur, 0);
+      partions.push(max + 1 - prev);
+    }
+  }
+
+  return partions;
+};
