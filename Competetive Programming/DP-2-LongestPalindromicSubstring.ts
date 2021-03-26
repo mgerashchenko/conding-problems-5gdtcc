4efe06a904cde3df4070407506df9cc13e05755e
mgerashchenko <mgerashchenko@gmail.com>
2021-03-26 13:08:22 -0400

    up

--- Competetive Programming/DP-2-LongestPalindromicSubstring.ts
+++ Competetive Programming/DP-2-LongestPalindromicSubstring.ts
@@ -1,46 +1,52 @@
 // 5.Longest Palindromic Substring
 // https://leetcode.com/problems/longest-palindromic-substring/
 
+// DP Solution
+// https://www.youtube.com/watch?v=UflHuQj6MVA
+
+// ! Not optimized but easy to understand
+
 /**
  * @param {string} s
  * @return {string}
  */
 var longestPalindrome = function(s) {
-  let dp = {};
-  let res = "";
-
-  // Not effective way to find a palindrom
-  const isPalindrom = sub => {
-    if (sub.length === 1) return true;
-    let middle = Math.floor(sub.length / 2 - 1);
-    for (let i = 0; i < middle; i++) {
-      if (sub[i] !== sub[sub.length - 1 - i]) return false;
-      // optimize the solution by saving the middle solution
-      dp[sub.slice(i, sub.length - i)] = true;
-    }
-    return true;
-  };
-
-  let i = 0;
-  let start = i;
-  let end = i + 1;
-  while (i < s.length) {
-    let sub = s.slice(start, end);
-    dp[sub] = dp[sub] == null ? isPalindrom(s) : dp[sub];
-    console.log(sub);
-    if (dp[sub]) {
-      console.log("in", sub);
-      res = sub.length > res.length ? sub : res;
-      if (start - 1 >= 0 && end + 1 <= s.length) {
-        start--;
-        end++;
-        continue;
-      }
-    }
-    i++;
-    start = i;
-    end = i + 1;
+  let max = s.slice(0, 1);
+  // dp[] - represents start
+  // dp[][] - represents end
+  let dp = [];
+  for (let i = 0; i < s.length; i++) {
+    dp.push([]);
   }
 
-  return res;
+  // fill the diagolan means that
+  // all single char el are polindrom
+  for (let i = 0; i < s.length; i++) {
+    dp[i][i] = 1;
+  }
+
+  // fill palidroms of length 2
+  for (let i = 0; i < s.length; i++) {
+    dp[i][i + 1] = s[i] === s[i + 1];
+    if (dp[i][i + 1] && max.length < 2) {
+      max = s.slice(i, i + 1 + 1);
+    }
+  }
+
+  // fill the res of the matrix
+  for (let n = 2; n < s.length; n++) {
+    for (let i = 0; i < s.length - 2; i++) {
+      // if start and end chars are equal
+      // and sub in the middle is palindrom
+      // string is a palindrom
+      if (s[i] === s[i + n] && dp[i + 1][i + n - 1]) {
+        dp[i][i + n] = true;
+        if (n > max.length - 1) {
+          max = s.slice(i, i + n + 1);
+        }
+      }
+    }
+  }
+
+  return max;
 };
