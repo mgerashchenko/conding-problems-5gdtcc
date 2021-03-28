3080fb3c51c2901932aab63a023ca033f8a24ad2
mgerashchenko <mgerashchenko@gmail.com>
2021-03-28 15:00:24 -0400

    BTR-1

---
+++ Competetive Programming/BTR-1.ts
@@ -0,0 +1,9 @@
+// BACK TRACKING
+// https://epam.sharepoint.com/sites/CompetitiveProgramming/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FCompetitiveProgramming%2FShared%20Documents%2F08%2D22%2D2020%2FRecordings%2FMeeting%20in%20%5FBacktracking%5F%2D20210327%5F100719%2DMeeting%20Recording%2Emp4&parent=%2Fsites%2FCompetitiveProgramming%2FShared%20Documents%2F08%2D22%2D2020%2FRecordings
+
+// Back Tracking - looking for all posible solutions
+//    example: all posible combinations for 'AXZ', 6(3!=1*2*3) total with a Tree
+// DFS           - special case of back tracking for search in tree structures
+
+// 797. All Paths From Source to Target
+// https://leetcode.com/problems/all-paths-from-source-to-target/
