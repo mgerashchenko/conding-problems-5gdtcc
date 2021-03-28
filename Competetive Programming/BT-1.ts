39899fa55a97964a087c020c18aafe148fd12e45
mgerashchenko <mgerashchenko@gmail.com>
2021-03-28 16:16:17 -0400

    BT-1

--- Competetive Programming/BT-1.ts
+++ Competetive Programming/BT-1.ts
@@ -7,3 +7,33 @@
 
 // 797. All Paths From Source to Target
 // https://leetcode.com/problems/all-paths-from-source-to-target/
+
+// Guaranteed Directed acyclic graph(DAG)
+
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+var allPathsSourceTarget = function(graph) {
+    let res = [];
+
+    (function BT(path, index) {
+        if(index === graph.length - 1) {
+            res.push([...path]);
+            return;
+        }
+
+        for(let newIndex of graph[index]) {
+            path.push(newIndex)
+            BT(path, newIndex);
+            // back tracking here, by removing 
+            // visted node because of the clouser
+            path.pop();
+            
+            // or can do
+            // BT([...path, newIndex], newIndex);
+        }
+    })([0], 0)
+
+    return res;
+};
