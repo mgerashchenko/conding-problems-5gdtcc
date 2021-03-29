5df1c5d435b9e96b2f902071ff5b1b9aff39bd88
mgerashchenko <mgerashchenko@gmail.com>
2021-03-28 21:35:58 -0400

    BT-1

--- Competetive Programming/BT-1-SequentialDigits.ts
+++ Competetive Programming/BT-1-SequentialDigits.ts
@@ -3,3 +3,6 @@
 
 // https://www.youtube.com/watch?v=sFt3KVGyeWw
 // Using a window of '123456789'
+
+//https://github.com/luliyucoordinate/Leetcode/blob/master/src/1291-Sequential-Digits/1291.java
+// Using the % for digit
