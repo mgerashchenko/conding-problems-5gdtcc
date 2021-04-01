ef0261e777b73b0d5d3d9c59a7a2990cbc7c15fa
mgerashchenko <mgerashchenko@gmail.com>
2021-03-31 23:10:31 -0400

    DP-2

--- Competetive Programming/DP-2-PalindromicSubstrings.ts
+++ Competetive Programming/DP-2-PalindromicSubstrings.ts
@@ -8,48 +8,23 @@
  * @return {number}
  */
 var countSubstrings = function(s) {
-  let res = 0;
+ let res = 0;
+    
+ let isPalindromFromTheMiddle = (start, end) => {
+     // grow from the middle
+     while(start>=0 && end<s.length && s[start] === s[end]){
+         res++;
+         end++;
+         start--;
+     }
+ }
 
-  const isPalindrom = str => {
-    let i = 0;
-    if (str.length === 0) {
-      return 0;
-    }
-    if (str.length === 1) {
-      return 1;
-    }
-    if ((str.length === 2, str.length === 3)) {
-      return str[0] === str[str.length - 1] ? 1 : 0;
-    }
-    while (i < str.length - 1 - i) {
-      if (str[i] !== str[str.length - 1 - i]) {
-        return 0;
-      }
-      i++;
-    }
-    return 1;
-  };
-
-  // No Memoization(top-down)
-  // Runtime: 452 ms Memory Usage: 44.7 MB
-  const cash = new Map();
-  for (let i = 0; i < s.length; i++) {
-    for (let j = 0; j < s.length; j++) {
-      res += isPalindrom(s.slice(i, s.length - j));
-    }
-  }
-
-  // Memoization(top-down)
-  // Runtime: 2008 ms, Memory Usage: 101.4 MB
-  // const cash = new Map();
-  // for(let i=0;i<s.length;i++){
-  //     for(let j=0;j<s.length;j++){
-  //         if(cash.get(s.slice(i,s.length-j)) == null){
-  //             cash.set(s.slice(i,s.length-j), isPalindrom(s.slice(i,s.length-j)));
-  //         }
-  //         res += cash.get(s.slice(i,s.length-j));
-  //     }
-  // }
-
-  return res;
+ for(let i=0;i<s.length;i++) {
+     // aba case
+     isPalindromFromTheMiddle(i,i);
+     // aa case
+     isPalindromFromTheMiddle(i,i+1);
+ }
+    
+ return res;
 };
