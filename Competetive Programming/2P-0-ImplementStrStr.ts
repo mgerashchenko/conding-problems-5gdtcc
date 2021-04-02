e1cad2275d8c3c0f6c831af75c56e87b7a9449ee
mgerashchenko <mgerashchenko@gmail.com>
2021-04-02 00:39:25 -0400

    2P-0

--- Competetive Programming/2P-0-ImplementStrStr.ts
+++ Competetive Programming/2P-0-ImplementStrStr.ts
@@ -3,3 +3,32 @@
 // Easy
 
 // Naive vol #0
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    // when haystack is '' return 0
+    // when needle is ''  retirn 0
+    // when there is match return -1
+    
+    // Constraints
+    //  0 <= haystack <= 5*10^4
+    // lower-case English letters
+    
+    if(needle.length===0) return 0;
+    // Good catch!!!
+    if(needle.length > haystack.length) return-1;
+    
+    for(let i=0;i<haystack.length;i++){
+        let j;
+        for(j=0;j<needle.length;j++){
+            if(haystack[i+j]!==needle[j]) break;
+        }
+        if(j===needle.length) return i;
+    }
+    
+    return -1;
+};
