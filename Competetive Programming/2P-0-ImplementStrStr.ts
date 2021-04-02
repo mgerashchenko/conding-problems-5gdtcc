35f13a554a242ac529a1b38bb7063625f8480355
mgerashchenko <mgerashchenko@gmail.com>
2021-04-02 00:41:58 -0400

    up

--- Competetive Programming/2P-0-ImplementStrStr.ts
+++ Competetive Programming/2P-0-ImplementStrStr.ts
@@ -2,7 +2,7 @@
 // Implement strStr
 // Easy
 
-// Naive vol #0
+// A aive vol #0
 
 /**
  * @param {string} haystack
@@ -10,25 +10,25 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    // when haystack is '' return 0
-    // when needle is ''  retirn 0
-    // when there is match return -1
-    
-    // Constraints
-    //  0 <= haystack <= 5*10^4
-    // lower-case English letters
-    
-    if(needle.length===0) return 0;
-    // Good catch!!!
-    if(needle.length > haystack.length) return-1;
-    
-    for(let i=0;i<haystack.length;i++){
-        let j;
-        for(j=0;j<needle.length;j++){
-            if(haystack[i+j]!==needle[j]) break;
-        }
-        if(j===needle.length) return i;
+  // when haystack is '' return 0
+  // when needle is ''  retirn 0
+  // when there is match return -1
+
+  // Constraints
+  //  0 <= haystack <= 5*10^4
+  // lower-case English letters
+
+  if (needle.length === 0) return 0;
+  // Good catch!!!
+  if (needle.length > haystack.length) return -1;
+
+  for (let i = 0; i < haystack.length; i++) {
+    let j;
+    for (j = 0; j < needle.length; j++) {
+      if (haystack[i + j] !== needle[j]) break;
     }
-    
-    return -1;
+    if (j === needle.length) return i;
+  }
+
+  return -1;
 };
