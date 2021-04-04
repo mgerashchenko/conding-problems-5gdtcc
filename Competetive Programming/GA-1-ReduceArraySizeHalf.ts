7d95c15dceaa7bd13e56d47a225eabb51a8f4329
mgerashchenko <mgerashchenko@gmail.com>
2021-04-03 20:48:18 -0400

    GA-1

---
+++ Competetive Programming/GA-1-ReduceArraySizeHalf.ts
@@ -0,0 +1,2 @@
+// Reduce Array Size to The Half
+// https://leetcode.com/problems/reduce-array-size-to-the-half/
--- Competetive Programming/GA-1-SumAbsoluteDifferencesSortedArray.ts
+++ Competetive Programming/GA-1-SumAbsoluteDifferencesSortedArray.ts
@@ -1,2 +1,2 @@
-/https://leetcode.com/problems/sum-of-absolute-differences-in-a-sorted-array/
+// https://leetcode.com/problems/sum-of-absolute-differences-in-a-sorted-array/
 // Sum of Absolute Differences in a Sorted Array
