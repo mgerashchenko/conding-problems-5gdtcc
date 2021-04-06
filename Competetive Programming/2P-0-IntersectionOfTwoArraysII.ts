7ec6a62dcf9eaf2499266906d954d39dbc277cb6
mgerashchenko <mgerashchenko@gmail.com>
2021-04-06 14:34:14 -0400

    2P  up

--- Competetive Programming/2P-0-IntersectionOfTwoArraysII.ts
+++ Competetive Programming/2P-0-IntersectionOfTwoArraysII.ts
@@ -1,33 +1,40 @@
-// #1
+// #2
 // https://leetcode.com/problems/intersection-of-two-arrays-ii/
 
 var intersect = function(nums1, nums2) {
-  // Constraints
-  // nums length 1<= n <= 1000
-  // nums i 1 <= i <= 1000
-  const res = [];
-
-  nums1.sort((x, y) => x - y);
-  nums2.sort((x, y) => x - y);
-
-  let [tmp1, tmp2] =
-    nums1.length < nums2.lenght ? [nums1, nums2] : [nums2, nums1];
-  let i = 0,
-    j = 0;
-  while (i < tmp1.length && j < tmp2.length) {
-    if (tmp1[i] === tmp2[j]) {
-      res.push(tmp1[i]);
-      i++;
-      j++;
-      continue;
+    // Constraints
+    // 1 <= lenght <= 1000
+    // 0 <= n[i] <= 1000
+    
+    const res = [];
+    
+    // I. Let's sort an array
+    let s1 = [...nums1].sort((x,y)=>x-y); // O(log^n)
+    let s2 = [...nums2].sort((x,y)=>x-y); // O(log^n)
+    
+    // Lest make the sort first
+    [s1, s2] = s1.length < s2.length ? [s1, s2] : [s2, s1];
+    
+    let [i,j]=[0,0];
+    while(i<s1.length && j<s2.length) {
+          if(s1[i]===s2[j]) {
+              res.push(s1[i]);
+              i++;
+              j++;
+              continue;
+          }
+          if(s1[i]<s2[j]){
+              // thre is no sence to j++
+              // cos it will be equlal or greater
+              i++;
+              continue;
+          }
+          // if shorter is greater than
+          // longer lood to the next longer
+          j++;
     }
-    if (tmp2[j] > tmp1[i]) {
-      i++;
-      continue;
-    }
-    j++;
-  }
-  return res;
+    
+    return res;
 };
 
-// for #2 write a hashMap
+// for # write a hashMap
