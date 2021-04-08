fc04db6975338b38cd3178c8a95b014a658b2133
mgerashchenko <mgerashchenko@gmail.com>
2021-04-07 23:17:58 -0400

    2P-0 up

--- Competetive Programming/2P-0-ImplementStrStr.ts
+++ Competetive Programming/2P-0-ImplementStrStr.ts
@@ -1,8 +1,11 @@
-// A naive vol #0
-
+// Vol #1 - GOOD 2p to practice indexes and conner casses
 // https://leetcode.com/problems/implement-strstr/
 // Implement strStr
 // Easy
+
+// Implement strStr().
+// Return the index of the first occurrence of needle in haystack, or -1 if needle is not
+// part of haystack.
 
 /**
  * @param {string} haystack
@@ -30,5 +33,63 @@ var strStr = function(haystack, needle) {
     if (j === needle.length) return i;
   }
 
+  return -1;
+};
+
+// #2 Optimized solution
+
+// Conner casses
+// 'aa' '' - easy win // 0
+// 'a' 'a' - test algo for single value
+// 'hello' 'll' - base case
+// 'hlello' 'll' - need to reset the search when after first fail
+// 'mississippi' 'issip' - need to reset search index to last postion
+//                         to get ovelaped cases
+
+// 0 1 1 1 4
+// 0 1 2 3 4 5 6 7 8
+// m i s s i s s i p p i
+// 0 1 2 3 4 5
+// i s s i p
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  // Constraints
+  // 0 <= haystack.length <= 10^
+  // 0 <= needle.length <= 10^
+
+  // If need le is '' alwaus return 0;
+  if (needle.length === 0) return 0;
+
+  let last = 0,
+    i = 0,
+    j = 0;
+  while (i < haystack.length) {
+    if (haystack[i] === needle[j]) {
+      // when 1st match
+      if (j === 0) {
+        // remember the the last position
+        last = i;
+      }
+      // look to the next symbol
+      j++;
+    } else {
+      // when not match after the match
+      if (j > 0) {
+        // reset i to last and reset search
+        i = last;
+        j = 0;
+      }
+    }
+    // when is end of search
+    if (j === needle.length) return i - (needle.length - 1);
+    i++;
+  }
+
+  // if there is no match return -1;
   return -1;
 };
