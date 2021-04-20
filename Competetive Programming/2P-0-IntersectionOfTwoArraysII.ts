0ff71cb4918eb98e32b8e80ab365990af85510ae
mgerashchenko <mgerashchenko@gmail.com>
2021-04-19 22:12:46 -0400

    up

--- Competetive Programming/2P-0-IntersectionOfTwoArraysII.ts
+++ Competetive Programming/2P-0-IntersectionOfTwoArraysII.ts
@@ -2,52 +2,61 @@
 // https://leetcode.com/problems/intersection-of-two-arrays-ii/
 
 var intersect = function(nums1, nums2) {
-  // Constrains
-  // num.lenght [1..1000]
-  // num[i] [0...1000]
-
-  // it is not just intersection
-  // need to count amount of matches
-  // make sense to sort array first
-  // makse sense to start from the shorter array
-
-  //  0123
-  // [1221]
-  //  013
-  // [22]
-
-  //  01234
-  // [122345]
-  //  012
-  // [24]
-
-  const res = [];
-
-  // sort array first to optimize search
-  const sa1 = [...nums1].sort((x, y) => x - y); // O(n);
-  const sa2 = [...nums2].sort((x, y) => x - y);
-
-  // find the smallest array first
-  if (sa1.lenght > sa2.length) {
-    // swap arrays
-    [sa1, sa2] = [sa2, sa1];
-  }
-
-  let i = (j = 0);
-  while (i < sa1.length && j < sa2.length) {
-    if (sa1[i] === sa2[j]) {
-      res.push(sa1[i]);
-      i++;
-      j++;
-      continue;
+    // Constrains
+    // num.lenght [1..1000]
+    // num[i] [0...1000]
+    
+    // it is not just intersection
+    // need to count amount of matches
+    // make sense to sort array first
+    // makse sense to start from the shorter array
+      
+    //  0123
+    // [1221]
+    //  013
+    // [22]
+     
+    //  01234
+    // [122345]
+    //  012
+    // [24] 
+    
+    const res = [];
+    
+    // sort array first to optimize search
+    const sa1 = [...nums1].sort((x,y)=>x-y); // O(n);
+    const sa2 = [...nums2].sort((x,y)=>x-y);
+    
+    // find the smallest array first
+    if(sa1.lenght > sa2.length){
+        // swap arrays
+        [sa1, sa2] = [sa2, sa1];
     }
-    if (sa1[i] < sa2[j]) {
-      i++;
-      continue;
-    }
+    
+    let i=j=0;
+    while(i<sa1.length && j<sa2.length){
+        // When there is a match
+        if(sa1[i]===sa2[j]){
+            // update the result
+            res.push(sa1[i]);
+            // and go forward
+            i++;
+            j++;
+            continue;
+        }
+        // if the first array is smaller 
+        if(sa1[i]<sa2[j]){
+            // then increase the index of smaller array
+            i++;
+            continue;
+        }
 
-    j++;
-  }
+        // otherwise increase the index of the second array
+        j++;
+    }
+    
+    return res;
+};
 
   return res;
 };
