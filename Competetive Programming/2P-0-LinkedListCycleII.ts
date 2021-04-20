3a8e0f074f8eb15dcf6b24e3f1d5c6e4c9f5d9a3
mgerashchenko <mgerashchenko@gmail.com>
2021-04-19 23:13:32 -0400

    2P - up

--- Competetive Programming/2P-0-LinkedListCycleII.ts
+++ Competetive Programming/2P-0-LinkedListCycleII.ts
@@ -1,31 +1,37 @@
-// #
+// #1 - naive, space complexity is O(n)
 // https://leetcode.com/problems/linked-list-cycle-ii/
 // Linked List Cycle II
 // Medium
 
+// Lets do Floyds algo next time!
+// https://www.youtube.com/watch?v=9YTjXqqJEFE
+// It is about to have fast and slow.
+// then from start and meeting point iterate tmp = tmp.next
+// new meeting point will be the dublicate or the start of the loop
+
 var detectCycle = function(head) {
-    // Constraints
-    // length [0, 10]
-    // val [-10, 10]
-    // pos -1 or index [0,10]
-    // O(1) - memory
-    // don't modify the list
-    
-    // easy win
-    if(head == null || head.next == null) return null;
-    
-    let map = new Map(),
-        tmp = head,
-        pos = 0;
-    while(tmp != null){
-        // if node exist it is a loop
-        if(map.has(tmp)){
-            return tmp;
-        }
-        // remember the nodes in the map
-        map.set(tmp);
-        tmp = tmp.next;
+  // Constraints
+  // length [0, 10]
+  // val [-10, 10]
+  // pos -1 or index [0,10]
+  // O(1) - memory
+  // don't modify the list
+
+  // easy win
+  if (head == null || head.next == null) return null;
+
+  let map = new Map(),
+    tmp = head,
+    pos = 0;
+  while (tmp != null) {
+    // if node exist it is a loop
+    if (map.has(tmp)) {
+      return tmp;
     }
-    
-    return null;
+    // remember the nodes in the map
+    map.set(tmp);
+    tmp = tmp.next;
+  }
+
+  return null;
 };
