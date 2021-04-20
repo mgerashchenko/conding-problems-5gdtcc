f75c202a32c141c8c922310fd94e0850d437ac38
mgerashchenko <mgerashchenko@gmail.com>
2021-04-19 22:04:47 -0400

    2P-0 up

--- Competetive Programming/2P-0-BoatsToSavePeople.ts
+++ Competetive Programming/2P-0-BoatsToSavePeople.ts
@@ -2,24 +2,29 @@
 // https://leetcode.com/problems/boats-to-save-people/
 // Boats to Save People
 
+
 var numRescueBoats = function(people, limit) {
-  // Constraints
-  // 1 <= people length ...
-  // 1 <= people[i]
-  // 1 <= limit
-  // people[i] <= lim
-
-  let res = 0;
-  const peopleS = [...people].sort((x, y) => x - y); // O(n)
-
-  let i = 0,
-    j = peopleS.length - 1;
-  while (i <= j) {
-    res++;
-    if (peopleS[i] + peopleS[j] <= limit) {
-      i++;
+    // Constraints 
+    // 1 <= people length ...
+    // 1 <= people[i]
+    // 1 <= limit
+    // people[i] <= lim
+    
+    let res = 0;
+    // incremental
+    const peopleS = [...people].sort((x,y)=>x-y);
+    
+    let i=0,
+        j = peopleS.length-1;
+    while(i<=j){
+        res++;
+        // if we can add 1 more add if not
+        if(peopleS[i]+peopleS[j]<=limit) {
+            i++;
+        }
+        // use just the bigest number;
+        j--;
     }
-    j--;
-  }
-  return res;
-}; // O(n)
+    return res
+};
+
