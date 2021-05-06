04b99320390428e005fb7a3fac7267ea9ceaf43c
mgerashchenko <mgerashchenko@gmail.com>
2021-05-05 20:29:50 -0400

    SW-0 up

--- Competetive Programming/DFS-0-SmallestSubtreewithAllDeepestNodes.ts
+++ Competetive Programming/DFS-0-SmallestSubtreewithAllDeepestNodes.ts
@@ -1,5 +1,3 @@
 // #
 // https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/
 // Smallest Subtree with all the Deepest Nodes
-
-
--- Competetive Programming/SW-0-GrumpyBookstoreOwner.ts
+++ Competetive Programming/SW-0-GrumpyBookstoreOwner.ts
@@ -4,65 +4,51 @@
 
 // Use preindex next time!!!
 
-/**
- * @param {number[]} customers
- * @param {number[]} grumpy
- * @param {number} X
- * @return {number}
- */
 var maxSatisfied = function(customers, grumpy, X) {
   // Constraints
-  // 1 <= X <= 1000
-  // 0 <= customers[i] <= 1000
-  // 0 <= grumpy <= 1
+  // 0<=customers.length, grumpy.length<=1000
+  // 0<=customers[i] value<=1000
+  // 1<=X value<=customers.length
 
-  // Algorithm Sliding window
-  // find the sum of the existing customers
-  // - add value if grumpy is 0
-  // init the window, get the current sum for a start 0
-  // - add value if grumpy is 1
-  // compare the current sum with the max
-  // move the window to the right,
-  // - remove left value from cur if grumpy is 1
-  // - add right valye if cur is grupy is 1
-  // compare current and max
-  // return max
+  // calculate sum without superpower
 
-  // get sum without super power
+  // Algorigth Sliding window
+  // init the window value
+  // move the the window
+  // - substract left part
+  // - add right part
+
+  // sum without superpower
+
   let max = 0;
   for (let i = 0; i < customers.length; i++) {
     if (grumpy[i] === 0) {
       max += customers[i];
     }
-  } // O(n)
+  }
 
   // init the window
   for (let i = 0; i < X; i++) {
-    // if a superpower is useful
     if (grumpy[i] === 1) {
       max += customers[i];
     }
-  } // O(n)
+  }
 
-  // Move the widnow
-  let current = max,
-    start = 0,
-    end = X - 1;
-  while (end < customers.length) {
-    // if superpower was applied
+  // move the window
+  let cur = max;
+  let start = 0;
+  let end = X - 1;
+  while (end < customers.length - 1) {
     if (grumpy[start] === 1) {
-      // substract it
-      current -= customers[start];
+      cur -= customers[start];
     }
     start++;
     end++;
-    // if superpower is useful
     if (grumpy[end] === 1) {
-      // apply it
-      current += customers[end];
+      cur += customers[end];
     }
-    max = Math.max(current, max);
-  } // O(n)
+    max = Math.max(cur, max);
+  }
 
   return max;
 };
