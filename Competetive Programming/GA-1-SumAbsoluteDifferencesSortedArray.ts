60b3b4fc56e4c83dc17a9e2e5612da72f1cc775a
mgerashchenko <mgerashchenko@gmail.com>
2021-05-06 01:17:13 -0400

    GA-1

--- Competetive Programming/GA-1-SumAbsoluteDifferencesSortedArray.ts
+++ Competetive Programming/GA-1-SumAbsoluteDifferencesSortedArray.ts
@@ -7,21 +7,40 @@
  * @return {number[]}
  */
 var getSumAbsoluteDifferences = function(nums) {
-  // constraints
-  // 2 <= nums <= 10^5
-  // 1 <= nums[i] <= 10^4
+    // Constraints
+    // 2 <= nums <= 1000
+    // 1 <= nums[i] <= 1000
+    
+    // Naive approach is to n^2
+    // Advanced is for sorted array
+    
+    // calculate sum for all elements
+    // s1 + cur + s2
+    // s1 = n1*i-sum1
+    // s2 = sum2-n2*(n-i-1)
+    
 
-  let res = [],
-    n = nums.length,
-    // Instead of Math.abs(nums[i]-nums[j]) which O(n^2)
-    // We calculate prev nums[i]*n - sum and post sum - nums[i]*n-i
-    left = 0,
-    rigth = nums.reduce((prev, cur) => prev + cur, 0);
-  for (let i = 0; i < n; i++) {
-    left += nums[i - 1] || 0;
-    rigth -= nums[i];
-    res[i] = nums[i] * i - left + rigth - nums[i] * (n - (i + 1));
-  } // O(n)
+    // init sum
+    // let sum=0;
+    // for(let i=0;i<nums.length;i++) {
+    //     sum += nums[i];
+    // }
+    let sum = nums.reduce((prev,cur)=>prev+cur, 0);
 
-  return res;
-};
+    let ans = [],
+        left_sum = 0,
+        right_sum = sum,
+        left_n, right_n, value,
+        n = nums.length;
+    // calculate the values
+    for(let i=0; i<n;i++) {
+        let cur = nums[i];
+        right_sum = right_sum - cur;
+        left_n = i;
+        right_n = n-i-1;
+        ans[i] = left_n*cur-left_sum + right_sum-right_n*cur;
+        left_sum += nums[i];
+    }
+
+    return ans;
+ };
