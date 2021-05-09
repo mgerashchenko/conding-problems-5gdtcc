ec711fa7530c8e447a7f52c7f0fa49f429115885
mgerashchenko <mgerashchenko@gmail.com>
2021-05-08 22:45:58 -0400

    2P-2 up

--- Competetive Programming/2P-2-BagOfTokens.ts
+++ Competetive Programming/2P-2-BagOfTokens.ts
@@ -7,45 +7,50 @@
  * @return {number}
  */
 var bagOfTokensScore = function(tokens, P) {
-  // Constraints
-  // 0<=tekens.length<=100
-  // 0<=tocke<=100
-
-  // sort tokens
-  // if power less than smalles token and count is 0 return 0
-  // if power more than smallest token play it
-  //      and increment the count
-  // if the power is less than smallest and count is more than 0
-  //      and biggest token is not the last
-  //      play the biggest tocken
-  //      and decrement the count
-
-  tokens.sort((x, y) => x - y);
-
-  let small = 0,
-    big = tokens.length - 1,
-    count = 0;
-
-  // there at leas 1 tokens to play
-  while (small <= big) {
-    // When there is enogh power
-    if (P >= tokens[small]) {
-      // play and continue
-      P = P - tokens[small];
-      count++;
-      small++;
-      continue;
-    }
-
-    // no power or there are less than 2 tokens left
-    // coas there is no sens to change 1 to less than 2 tokens
-    if (count === 0 || small + 1 >= big) return count;
-
-    // use superpower
-    P += tokens[big];
-    big--;
-    count--;
-  } // T:O(n) S:O(n)
-
-  return count;
-}; // T:O(n) S:O(n)
+    // Constraints
+    // 0 <= tockens.length
+    // 0 <= P
+    
+    // Algorithm
+    // sort Array
+    // use 2 pointers small and big
+    // use small when power is bigger small
+    // use big when existing powers is not enough and 
+    //      count is > 0
+    // iterate array
+    
+    // conner case token length is []
+    // the token array is [100]
+    // when the exchange doesn't make sense, length is less than 2
+    
+    // sort the array
+    tokens.sort((x,y) => x-y);
+    
+    // iterate the array
+    let small=0,
+        big=tokens.length-1,
+        count=0;
+    while(small<=big) {
+        // easy win first
+        if(P>=tokens[small]){
+            P -= tokens[small];
+            small++;
+            count++;
+            continue;
+        }
+        
+        // need count for exchange
+        // and the diff should be at least 2
+        if(count>0 && small+1<big){
+            P += tokens[big];
+            count--;
+            big--;
+            continue
+        }
+        // bail out if the is no tokens and Power
+        return count;
+    } // O(n)
+    
+    // return the result
+    return count;
+} // O(n)
--- Competetive Programming/2P-2-ReverseLinkedListII.ts
+++ Competetive Programming/2P-2-ReverseLinkedListII.ts
@@ -3,4 +3,47 @@
 
 // https://www.youtube.com/watch?v=wk8-_M-2fzI
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function(head, left, right) {
+  // 1 <= n
+  // - <= val <= +
+  // 1 <= left <= right <= n
 
+  // Algorithm
+  // protect from contter case
+  // - creat dum node for the pre el
+  // set curent at lef position
+  // repeat right - left times;
+
+  // create dum node, init prev;
+  let dummy = { next: head };
+
+  let prev = dummy,
+    cur = prev;
+  for (let i = 0; i < left; i++) {
+    prev = cur;
+    cur = cur.next;
+  }
+
+  let next;
+  for (let i = 0; i < right - left; i++) {
+    next = cur.next;
+    cur.next = next.next;
+    next.next = prev.next;
+    prev.next = next;
+  }
+
+  return dummy.next;
+};
