99adaebd16ae57fa477f07bbe1bdf3c1fd57f91f
mgerashchenko <mgerashchenko@gmail.com>
2021-05-19 22:04:57 -0400

    DP up

--- LeetCode/DFS_LeafSimilarTrees.ts
+++ LeetCode/DFS_LeafSimilarTrees.ts
@@ -2,44 +2,44 @@
 // Leaf-Similar Trees
 
 var leafSimilar = function(root1, root2) {
-    // Constraints
-    // 1 <= node.length
-    // 0 <= node.value
-    
-    // Algorithm
-    // Find the sequence for both treis with dfs
-    // compair arrays
-    
-    const getSequence = root => {
-        let stack = [root],
-            leafs = [];
-        while(stack.length){
-            let {val, left, right} = stack.pop();
+  // Constraints
+  // 1 <= node.length
+  // 0 <= node.value
 
-            // base case
-            if(left == null && right == null){
-               leafs.push(val); 
-            };
+  // Algorithm
+  // Find the sequence for both treis with dfs
+  // compair arrays
 
-            // interate
-            right && stack.push(right);
-            left && stack.push(left);
-        }
-        
-        // return result
-        return leafs;
+  const getSequence = root => {
+    let stack = [root],
+      leafs = [];
+    while (stack.length) {
+      let { val, left, right } = stack.pop();
+
+      // base case
+      if (left == null && right == null) {
+        leafs.push(val);
+      }
+
+      // interate
+      right && stack.push(right);
+      left && stack.push(left);
     }
-    
-    const [leafs1, leafs2] = [root1, root2].map(getSequence);
-    
-    // case
-    if(leafs1.length !== leafs2.length) return false;
-    
-    // case
-    for(let i in leafs1){
-        if(leafs1[i]!==leafs2[i]) return false;
-    }
-    
-    // return value
-    return true;
+
+    // return result
+    return leafs;
+  };
+
+  const [leafs1, leafs2] = [root1, root2].map(getSequence);
+
+  // case
+  if (leafs1.length !== leafs2.length) return false;
+
+  // case
+  for (let i in leafs1) {
+    if (leafs1[i] !== leafs2[i]) return false;
+  }
+
+  // return value
+  return true;
 };
---
+++ LeetCode/DP_MinCostClimbingStairs.ts
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/min-cost-climbing-stairs/
+// 746. Min Cost Climbing Stairs
+
+// JSON
+var minCostClimbingStairs = function(cost) {    
+    // Constraings 
+    // 2 <= cost.length
+    
+    const dp = [cost[0], cost[1]];
+    
+    for(let i=2;i<cost.length;i++){
+        dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
+    }
+    
+    return Math.min(dp[dp.length-1], dp[dp.length-2]);
+};
+
+
+
+// Python
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        for i in range(2, len(cost)):
+            cost[i] += min(cost[i-1], cost[i-2])
+        return min(cost[-1], cost[-2])
