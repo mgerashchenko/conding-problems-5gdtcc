cb472cb9673b774992691e197e965e12f8c3f0d4
mgerashchenko <mgerashchenko@gmail.com>
2021-05-21 23:46:19 -0400

    DP up

--- LeetCode/DP_CountSortedVowelStrings.ts
+++ LeetCode/DP_CountSortedVowelStrings.ts
@@ -36,3 +36,16 @@ var countVowelStrings = function(n) {
 
 
 // Python
+
+// class Solution:
+//     def countVowelStrings(self, n: int) -> int:
+//         if n == 0:
+//             return 0
+        
+//         counts = [1]*5
+//         while n > 1 :
+//             n -= 1
+//             for i in reversed(range(len(counts)-1)):
+//                 counts[i] += counts[i+1];
+                
+//         return sum(counts)
