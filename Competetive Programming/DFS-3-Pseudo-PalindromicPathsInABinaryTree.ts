4bf89b187b8c1d6313f7df75245812f391397f24
mgerashchenko <mgerashchenko@gmail.com>
2021-05-24 22:42:11 -0400

    2P-1

--- Competetive Programming/DFS-3-Pseudo-PalindromicPathsInABinaryTree.ts
+++ Competetive Programming/DFS-3-Pseudo-PalindromicPathsInABinaryTree.ts
@@ -2,43 +2,43 @@
 // Pseudo-Palindromic Paths in a Binary Tree
 
 // Doesn't works
-var pseudoPalindromicPaths  = function(root) {
-    // Algorithm
-    // Use DFS
-    // Buld paths
-    // Check if it is pseudo palendromic
-    
-    if(!root) return 0;
-    
-    let paths = [];
-    
-    let stack = [{node: root, path: []}];
-    while(stack.length) {
-        let {node, path} = stack.pop();
-        
-        let {val, left, right} = node;
-        left && stack.push({node: left, path:[...path, val]});
-        right && stack.push({node: right, path:[...path, val]});
+var pseudoPalindromicPaths = function(root) {
+  // Algorithm
+  // Use DFS
+  // Buld paths
+  // Check if it is pseudo palendromic
 
-        if(!left && !right) {
-           paths.push([...path, val]);
-           continue;
-        }
-    }
-    
-    const isPsPalidromic = path => {
-        let hashMap = {};
-        for(let el of path){
-            if(hashMap[el] == null) {
-                hashMap[el] = 0;
-            }
+  if (!root) return 0;
 
-            hashMap[el]++;
-        }
-        
-        let counter = Object.keys(hashMap).reduce((prev, el) => hashMap[el]%2===1 ? prev+1 : prev, 0);
-        return counter<=1;
+  const isPsPalidromic = path => {
+    let hashMap = Array(10).fill(0);
+    for (let el of path) {
+      hashMap[el]++;
     }
-    
-    return paths.reduce((prev, path) => isPsPalidromic(path) ? prev+1: prev, 0);
+
+    let counter = 0;
+    for (let el of hashMap) {
+      el % 2 && counter++;
+
+      if (counter > 1) return false;
+    }
+
+    return true;
+  };
+
+  let count = 0;
+  let stack = [{ node: root, path: [] }];
+  while (stack.length) {
+    let { node, path } = stack.pop();
+
+    let { val, left, right } = node;
+    left && stack.push({ node: left, path: [...path, val] });
+    right && stack.push({ node: right, path: [...path, val] });
+
+    if (!left && !right) {
+      isPsPalidromic([...path, val]) && count++;
+    }
+  }
+
+  return count;
 };
