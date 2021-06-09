626698470f1a6fe1c1fbd45414e390eca286d994
mgerashchenko <mgerashchenko@gmail.com>
2021-06-09 09:12:59 -0400

    SW - LongestSubstring

--- Grokking the Coding Interview/SW_LongestSubstring.ts
+++ Grokking the Coding Interview/SW_LongestSubstring.ts
@@ -1,3 +1,28 @@
 // https://www.educative.io/courses/grokking-the-coding-interview/YQQwQMWLx80
 // Longest Substring
 
+const longest_substring_with_k_distinct = function(str, k) {
+  let max=0;
+
+  let map = {}, start=0;
+  // Object keys return the array
+  for(let end=0;end<str.length;end++){
+    if(map[str[end]] == null){
+      map[str[end]]=0;
+    }
+    map[str[end]]+= 1;
+
+    while(Object.keys(map).length>k){
+      map[str[start]] -= 1;
+      if (map[str[start]] === 0) {
+        delete map[str[start]];
+      }
+      start += 1; // shrink the window
+    }
+
+    max = Math.max(max, end-start+1);
+  }
+
+  return max;
+};
+
