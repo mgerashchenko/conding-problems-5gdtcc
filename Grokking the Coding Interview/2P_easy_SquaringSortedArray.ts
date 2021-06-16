dc694ec5f10321aa8767984a47140f685fdb3b15
mgerashchenko <mgerashchenko@gmail.com>
2021-06-16 10:37:01 -0400

    2P medium

--- Grokking the Coding Interview/2P_easy_SquaringSortedArray.ts
+++ Grokking the Coding Interview/2P_easy_SquaringSortedArray.ts
@@ -1,3 +1,31 @@
 // https://www.educative.io/courses/grokking-the-coding-interview/R1ppNG3nV9R
 // Squaring a Sorted Array
 
+const make_squares = function(arr) {
+  // two pointers from - and +
+  // fill array in reverse order
+
+  let resArr = [],
+    lastIndex = arr.length - 1;
+
+  let start = 0,
+    end = arr.length - 1;
+  while (start <= end) {
+    let startNum = Math.pow(arr[start], 2),
+      endNum = Math.pow(arr[end], 2);
+
+    if (startNum >= endNum) {
+      resArr[lastIndex] = startNum;
+      start++;
+    }
+
+    if (startNum < endNum) {
+      resArr[lastIndex] = endNum;
+      end--;
+    }
+
+    lastIndex--;
+  }
+
+  return resArr;
+}; // O(N) O(1)
---
+++ Grokking the Coding Interview/2P_medium_TripletSumToZero.ts
@@ -0,0 +1,93 @@
+// https://www.educative.io/courses/grokking-the-coding-interview/gxk639mrr5r
+// Triplet Sum to Zero
+
+const search_triplets = function(arr) {
+  // sort array
+  // third loop
+  // 2 pointers
+  // remove dublicates
+
+  let result = [];
+
+  arr.sort((x, y) => x - y);
+
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] === arr[i + 1]) continue;
+
+    let num = arr[i],
+        start = i + 1,
+        end = arr.length - 1;
+
+    while(start < end) {
+        // console.log(i,num,start, arr[start], end, arr[end])        
+
+        let sum = num + arr[start] + arr[end];
+        if(sum === 0) {
+          result.push([num, arr[start], arr[end]]);
+          start++;
+          end--;
+
+          while(start < end && arr[start] === arr[start-1]) {
+            start++;
+          };
+
+          while(start < end && arr[end] === arr[end+1]) {
+            end--;
+          };
+        }
+
+        if(sum > 0) {
+          end--;
+        }
+
+        if(sum < 0) {
+          start++;
+        }
+    }
+  }
+
+  return result;
+};
+
+// def search_triplets(arr):
+//   # sort array
+//   # iterate third
+//   # 2 pointers for the rest
+//   # add and skip dublicats
+
+//   result = []
+
+//   arr.sort();
+
+//   for i in range(len(arr)):
+//     num = arr[i]
+
+//     start = i + 1
+//     end = len(arr) - 1
+//     while start < end:
+//       sum_cur = arr[i] + arr[start] + arr[end]
+
+//       # : at the end
+//       if sum_cur == 0:
+//         result.append([num, arr[start], arr[end]])
+//         start += 1
+//         end -= 1
+
+//         # : check typical mistake
+//         while start < end & arr[start] == arr[start-1]:
+//           start += 1
+
+//         # : at the end
+//         # == not =
+//         while start < end & arr[end] == arr[end + 1]:
+//             end -= 1
+
+//       # : at the end
+//       if sum_cur > 0:
+//         end -= 1
+
+//       # : at the end
+//       if sum_cur < 0:
+//         start += 1
+
+//   return result
