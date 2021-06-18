72a608f0760661e61551441487a1ba3041f9f09e
mgerashchenko <mgerashchenko@gmail.com>
2021-06-18 13:14:15 -0400

    SW - up

--- Grokking the Coding Interview/SW_LongestSubstring.ts
+++ Grokking the Coding Interview/SW_LongestSubstring.ts
@@ -2,27 +2,22 @@
 // Longest Substring
 
 const longest_substring_with_k_distinct = function(str, k) {
-  let max = 0;
+  // max length
+  // sliding window
+  // hash map count
+  // hash map keys count
 
-  let map = {},
-    start = 0;
-  // Object keys return the array
-  for (let end = 0; end < str.length; end++) {
-    if (map[str[end]] == null) {
-      map[str[end]] = 0;
-    }
-    map[str[end]] += 1;
+  let maxLength = 0;
 
-    while (Object.keys(map).length > k) {
-      map[str[start]] -= 1;
-      if (map[str[start]] === 0) {
-        delete map[str[start]];
-      }
-      start += 1; // shrink the window
-    }
+  let hasMapCount = {};
 
-    max = Math.max(max, end - start + 1);
+  let start = 0;
+  for(let end=0;end<s.length;end++){
+    let endChar = str[end];
+    if(hasMapCount[endChar] == null
+      ? 1
+      : hasMapCount[endChar] + 1;
   }
 
-  return max;
-}; // O(N + N) == O(N)
+  return maxLength;
+};// O(N + N) == O(N)
