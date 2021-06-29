4fe5fa79ec9d97448e8a7a1d0975f5d8f31b7f75
mgerashchenko <mgerashchenko@gmail.com>
2021-06-29 10:05:31 -0400

    2P up

--- Grokking the Coding Interview/2P_medium_TripletSumToZero.ts
+++ Grokking the Coding Interview/2P_medium_TripletSumToZero.ts
@@ -2,53 +2,53 @@
 // Triplet Sum to Zero
 
 const search_triplets = function(arr) {
-  // init result
+  // find all unque triplets with sum === 0, in unsored array
+  // init results
   // sort array
   // iterate array
-  // skip with if i === i - 1 elements
-  // use 2 pointers from 2 sides, start = i +1, end = 0
-  // skip with while start === start - 1 elements
-  // return result
+  // skip duplicates, if i == i-1
+  // use 2 pointers from 2 sides
+  // add to result if summ is equal 0;
+  // skip start duplicates if start-1 == start
+  // skip end duplicates if end == end+1
+  // return results
 
-  // init result
-  let result = [];
+  // init results
+  let triplets = [];
 
   // sort array
-  arr.sort((x, y) => x - y);
+  arr.sort((x,y)=>x-y);
 
-  // iterate i
-  for (let i = 0; i < arr.length - 2; i++) {
-    // skip dublicates i
-    if (i > 0 && arr[i - 1] === arr[i]) continue;
+  // iterate array
+  for(let i=0;i<arr.length-2;i++){
 
-    let start = i + 1;
-    // !!! last index not the length
-    let end = arr.length - 1;
-    // <, coz we need 2 numbers
-    while (start < end) {
-      // base case
-      let sum = arr[i] + arr[start] + arr[end];
-      if (sum === 0) {
-        result.push([arr[i], arr[start], arr[end]]);
+    // skip duplicate for 1rst num
+    if(i>0 && arr[i-1] == arr[i]) i++;
+
+    // use 2 pointers from 2 sides
+    let start = i+1;
+    let end = arr.length-1;
+    while(start < end) {
+      if(arr[i]+arr[start]+arr[end] === 0){
+        // !!! push arr []
+        triplets.push([arr[i], arr[start], arr[end]]);
         start++;
         end--;
 
-        // // skip start dublicates
-        // while(start < end && arr[start-1] == arr[start]) start++
-
-        // // skip end dublicates
-        // while(start < end && arr[end] == arr[end+1]) end--;
+        // skip duplicates for start
+        while(start < end && arr[start-1] == arr[start]) start++;
+        while(start < end && arr[end] == arr[end+1]) end--;
       }
 
-      // !!! i is negative end is always positive
-      if (sum > 0) end--;
-      if (sum < 0) start++;
+      // !!! arr[i] is negative
+      if(arr[start]+arr[end] < -arr[i]) start++;
+      if(arr[start]+arr[end] > -arr[i]) end--;
     }
   }
 
-  // return ressult
-  return result;
-}; // Sorting O(N logN) + third iterate O(N) * 2 pointers (N) === O(N2) + O(N2) === O(N2)
+  // return result
+  return triplets;
+}; // O(N log N) + O(N^2) === O(N2); O(N) - required for sorting
 
 // def search_triplets(arr):
 //   # sort array
