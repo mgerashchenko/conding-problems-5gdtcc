037b83f581a7682f01bd8693f3beb6e6be0be3df
mgerashchenko <mgerashchenko@gmail.com>
2021-06-30 09:49:09 -0400

    FS Palindrome Linked List

---
+++ Grokking the Coding Interview/FS_PalindromeLinkedList.ts
@@ -0,0 +1,6 @@
+// https://www.educative.io/courses/grokking-the-coding-interview/B1PzmqOKDLQ
+// Palindrome LinkedList
+
+// Given the head of a Singly LinkedList, write a method to check if the LinkedList is a palindrome or not.
+// Your algorithm should use constant space and the input LinkedList should be in the original form once the algorithm is finished. The algorithm should have O(N)O(N) time complexity where ‘N’ is the number of nodes in the LinkedList.
+
