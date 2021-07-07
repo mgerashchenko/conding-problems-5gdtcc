52255ad01be680d32d4544eca494ccb9e263980e
mgerashchenko <mgerashchenko@gmail.com>
2021-07-07 11:21:45 -0400

    SW hard up

---
+++ Grokking the Coding Interview/SW_hard_StringAnagrams.ts
@@ -0,0 +1,64 @@
+// https://www.educative.io/courses/grokking-the-coding-interview/YQ8N2OZq0VM
+// String Anagrams
+
+// Given a string and a pattern, find all anagrams of the pattern in the given string.
+
+// Every anagram is a permutation of a string. As we know, when we are not allowed to repeat characters while finding permutations of a string, we get N!N! permutations (or anagrams) of a string having NN characters. For example, here are the six anagrams of the string “abc”:
+
+const find_string_anagrams = function(str, pattern) {
+  // find all anagrams of the pattern in the given string
+  // anagram is pemutation with no repeated chars
+  
+  // use 2 pointers from 1 side, as sliding window
+  // use hashMap to count chars in pattern
+  // use matchCounter to count matches
+   
+  // init hashMap and count chars
+  // iterate array
+  // increment end
+  // if char in pattern, decrement char count 
+  // and increment matchCount if hashMap count === 0
+  // if end - start + 1 > pattern length
+  // move start, if start char in pattern and === 0 decrement matchCount
+  // and increment hashMap char count
+  // if match count === pattern chars count
+  // add start to the result
+  // return result
+
+  let result_indexes = [];
+
+  let hashMap = {};
+  let matchCount = 0;
+
+  // init hashMap
+  for(let i=0;i<pattern.length;i++) {
+    let char = pattern[i];
+    if(!(char in hashMap)) {
+      hashMap[char] = 0;
+    }
+    hashMap[char]++;
+  }
+
+  let start = 0;
+  for(let end=0; end<str.length; end++){
+    let charEnd = str[end];
+
+    if(charEnd in hashMap) {
+      hashMap[charEnd]--;
+      if(hashMap[charEnd]===0) matchCount++;
+    }
+
+    if(end - start + 1 > pattern.length) {
+      let charStart = str[start];
+      if(hashMap[charStart] === 0) matchCount--;
+      if(charStart in hashMap) hashMap[charStart]++;
+      start++;
+    }
+
+    if(matchCount === Object.keys(hashMap).length){
+      result_indexes.push(start);
+    }
+  }
+
+  return result_indexes;
+};
