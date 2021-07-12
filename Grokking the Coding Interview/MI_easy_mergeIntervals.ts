de2ff93f9b353624601e717c85dea3c17ccea5ec
mgerashchenko <mgerashchenko@gmail.com>
2021-07-11 22:56:04 -0400

    MI update

--- Grokking the Coding Interview/MI_easy_mergeIntervals.ts
+++ Grokking the Coding Interview/MI_easy_mergeIntervals.ts
@@ -2,49 +2,43 @@
 // Merge Intervals
 //
 const merge = function(intervals) {
-  // merge intervals
-  // sort intervals by start
+  // there are intervals merge them
+  // 
+  // init merged = [];
+  // sort intevals N Log N
+  // 
   // iterate intervals
-  // have temo start and end
-  // if cur_start > tmo_end, safa tmp interval
-  // if not use min start and max end
+  // start = i1.start
+  // end = i2.start
+  // set tmp start and end
+  // if tmp start > end, push interval to result
+  // if not update end = min(end, tmp.end)
+  // return merged
 
-  // [[1,4], [2,5], [7,9]]
-  // t_s 2
-  // t_e 5
-  // s 7
-  // e 9
-  // [[1,5], [7,9]]
-
-  // conercase
-  if (intervals.length < 2) return intervals;
+  if(intervals.length < 2) return intervals;
 
   // init result
   let merged = [];
 
-  // sort.intervals
-  intervals.sort((x, y) => x.start - y.start);
+  // sort array
+  intervals.sort((x,y) => x.start - y.start);
 
-  // iterate intervals
-  let start = Infinity;
-  let end = -1;
-  for (let i = 0; i < intervals.length; i++) {
-    let { start: tmp_start, end: tmp_end } = intervals[i];
+  let {start, end} = intervals[0];
 
-    if (end != -1 && tmp_start > end) {
+  for(let i=1;i<intervals.length;i++) {
+    let {start:tmpStart, end:tmpEnd} = intervals[i];
+
+    if(end < tmpStart) {
       merged.push(new Interval(start, end));
-      start = tmp_start;
-      end = tmp_end;
+      start = tmpStart;
+      end = tmpEnd;
       continue;
     }
-
-    // update start and end
-    start = Math.min(start, tmp_start);
-    end = Math.max(end, tmp_end);
+    end = Math.max(end, tmpEnd);
   }
 
   // add the last one
   merged.push(new Interval(start, end));
 
   return merged;
-}; // O(N logN) + O(N); O(N)
+}; // O(N LogN + N)
