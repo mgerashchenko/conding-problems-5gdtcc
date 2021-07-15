822c809df08f3e1343e55d41a566a93822d369d5
mgerashchenko <mgerashchenko@gmail.com>
2021-07-14 22:34:15 -0400

    SW - up

--- Grokking the Coding Interview/SW_hard_SmallestWindowWithSubstring.ts
+++ Grokking the Coding Interview/SW_hard_SmallestWindowWithSubstring.ts
@@ -10,21 +10,68 @@
 
 const find_substring = function(str, pattern) {
   // Find the shortest substring, when string has all chars from the pattern
-  //
+  // 
   // use 2 pointers from 1 side as a sliding window
   // use hashMap to count pattern chars
   // use match counter to know when the pattern is matched
-  //
-  // init minStar, minEnd
-  //
+  // 
+  // init minStar
+  // 
   // init hashMap and count the patter chars
   // iterate the string
   // if end is in the hashMap decrement hashMap count
   // if hashMap end count === 0, increment match count
-  // while matchCount === 0,
-  // and update minLength = min(minEnd-minStart+1, end-start+1)
+  // while matchCount === 0, 
+  // and update minLength = min(minStart+minLength, end-start+1)
   // and start++
   // and hashMap(start) in hashMap, hashMap start --;
   // return if Infinity return "", or slice(start, end+1)
-  
-};
+
+  let minLength = Infinity;
+  let minStart = 0;
+
+  // init hashMapCount
+  let hashMapCount = {};
+  for(let i=0; i<pattern.length; i++){
+    let char = pattern[i];
+    if(!(char in hashMapCount)) {
+      hashMapCount[char] = 0;
+    }
+    hashMapCount[char]++;
+  }
+
+  let matchCount = 0;
+
+  // iterate the array
+  let start = 0;
+  for(end=0; end<str.length; end++) {
+    let endChar = str[end];
+
+    if(endChar in hashMapCount){
+      hashMapCount[endChar]--;
+      if(hashMapCount[endChar] === 0){
+        matchCount++;
+      }
+    }
+
+    while(matchCount === Object.keys(hashMapCount).length){
+      // update min length
+      if(end-start+1 < minLength){
+        minLength = end-start+1;
+        minStart = start;
+      }
+
+      let startChar = str[start];
+      if(startChar in hashMapCount){
+        if(hashMapCount[startChar]===0){
+          matchCount--;
+        }
+        hashMapCount[startChar]++;
+      }
+
+      start++;
+    }
+  }
+
+  return minLength == Infinity ? '' : str.slice(minStart, minStart + minLength);
+} // O(N + N) // O(P)
